88febc38fe42daa0fc4b7ffaefbc63964795bbfe
Blindside Studios <64647061+Blindside-Studios@users.noreply.github.com>
2024-02-09 02:16:00 +0100

    Initial Commit

---
+++ SlideEye/SlideEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideEyeApp.swift
+//  SlideEye
+//
+//  Created by Nicolas Helbig on 09.02.24.
+//
+
+import SwiftUI
+
+@main
+struct SlideEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
