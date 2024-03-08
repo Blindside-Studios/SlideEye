e7a6865f4865b935e60ae8b64f96a46d0e1dbe30
Blindside Studios <64647061+Blindside-Studios@users.noreply.github.com>
2024-03-08 20:50:22 +0100

    Fixed display issue
    In the friends adder UI, the background is no loner cut off at the top

--- SlideEye/Views/Pages/NewFriendPage.swift
+++ SlideEye/Views/Pages/NewFriendPage.swift
@@ -12,67 +12,70 @@ struct NewFriendPage: View {
     var body: some View {
         List
         {
-            Section("Name"){
-                TextField("Name", text: $friendDetails.name)
-            }
-            
-            Section("Profile Picture"){
-                friendDetails.profilePicture
-                    .resizable()
-                    .scaledToFit()
-                    .overlay(alignment: .bottomTrailing) {
-                            /*PhotosPicker(selection: $friendDetails.profilePicture,
-                                         matching: .images,
-                                         photoLibrary: .shared()) {
-                                Image(systemName: "pencil.circle.fill")
-                                    .symbolRenderingMode(.multicolor)
-                                    .font(.system(size: 30))
-                                    .foregroundColor(.accentColor)
-                            }
-                            .buttonStyle(.borderless)*/
-                        }
-            }
-            
-            Section("Occupation"){
-                TextField("Occupation", text: $friendDetails.occupation)
-            }
+            Group
+            {
+                Section("Name"){
+                    TextField("Name", text: $friendDetails.name)
+                }
+                .padding(.vertical, 30)
+                .offset(y: 30)
                 
-            Section("Location"){
-                TextField("Location", text: $friendDetails.location)
-            }
-            
-            Section(header: Text("City"), footer: Text("Enter the closest larger city your friend lives in, it will be used to find some of the information below")){
-                TextField("City", text: $friendDetails.city)
-                    .onSubmit {
-                        findCityDetails()
-                    }
-            }
-            
-            Section("Country"){
-                TextField("Country", text: $friendDetails.country)
-            }
-            
-            Section("Continent"){
-                TextField("Continent", text: $friendDetails.continent)
-            }
-            
-            Section(header: Text("Time Zone ID"), footer: Text("This should conform to the following format: Continent/City, e.g. 'America/New_York'")){
-                TextField("Time Zone ID", text: $friendDetails.timeZoneID)
-            }
-            
-            Section("Favorite"){
-                Toggle("Favorite", isOn: $friendDetails.isFavorite)
-            }
-            
-            Section("Notes"){
-                TextField("Notes", text: $friendDetails.notes)
+                Section("Profile Picture"){
+                    friendDetails.profilePicture
+                        .resizable()
+                        .scaledToFit()
+                        .overlay(alignment: .bottomTrailing) {
+                            /*PhotosPicker(selection: $friendDetails.profilePicture,
+                             matching: .images,
+                             photoLibrary: .shared()) {
+                             Image(systemName: "pencil.circle.fill")
+                             .symbolRenderingMode(.multicolor)
+                             .font(.system(size: 30))
+                             .foregroundColor(.accentColor)
+                             }
+                             .buttonStyle(.borderless)*/
+                        }
+                }
+                
+                Section("Occupation"){
+                    TextField("Occupation", text: $friendDetails.occupation)
+                }
+                
+                Section("Location"){
+                    TextField("Location", text: $friendDetails.location)
+                }
+                
+                Section(header: Text("City"), footer: Text("Enter the closest larger city your friend lives in, it will be used to find some of the information below")){
+                    TextField("City", text: $friendDetails.city)
+                        .onSubmit {
+                            findCityDetails()
+                        }
+                }
+                
+                Section("Country"){
+                    TextField("Country", text: $friendDetails.country)
+                }
+                
+                Section("Continent"){
+                    TextField("Continent", text: $friendDetails.continent)
+                }
+                
+                Section(header: Text("Time Zone ID"), footer: Text("This should conform to the following format: Continent/City, e.g. 'America/New_York'")){
+                    TextField("Time Zone ID", text: $friendDetails.timeZoneID)
+                }
+                
+                Section("Favorite"){
+                    Toggle("Favorite", isOn: $friendDetails.isFavorite)
+                }
+                
+                Section("Notes"){
+                    TextField("Notes", text: $friendDetails.notes)
+                }
             }
         }
-        .padding(.vertical, 35)
-        .offset(y: 35)
     }
 }
 
-/*#Preview {
+#Preview {
     NewFriendPage(friendDetails: .constant(ModelData().friends[0]))
-}*/
+}
